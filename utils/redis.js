fcf48bab4c0277b45f89bc906156c4187ab144a7
Menor25 <ajirimenor@gmail.com>
2023-03-08 19:01:46 +0100

    Inside the folder utils, create a file redis.js that contains the class RedisClient

--- utils/redis.js
+++ utils/redis.js
@@ -1,32 +1,34 @@
-import createClient from 'redis';
+import { createClient } from 'redis';
+import { promisify } from 'util';
+import dotenv from 'dotenv';
 
+dotenv.config();
 
 class RedisClient {
-    constructor() {
-        const host = process.env.RD_HOST || 'localhost';
-        const port = process.env.RD_PORT || '6379';
-        this.client = createClient({ host, port });
-        this.get = promisify(this.client.get).bind(this.client);
-        this.client.on('error', (err) => console.log(err.message));
-      }
+  constructor() {
+    const host = process.env.RD_HOST || 'localhost';
+    const port = process.env.RD_PORT || '6379';
+    this.client = createClient({ host, port });
+    this.get = promisify(this.client.get).bind(this.client);
+    this.client.on('error', (err) => console.log(err.message));
+  }
 
-    
-      isAlive() {
-        return this.client.connected;
-      }
+  isAlive() {
+    return this.client.connected;
+  }
 
-      async get(key) {
-        const value = await this.get(key);
-        return value;
-      }
+  async get(key) {
+    const value = await this.get(key);
+    return value;
+  }
 
-      async set(key, value, duration) {
-        await this.client.setex(key, duration, value);
-      }
+  async set(key, value, duration) {
+    this.client.setex(key, duration, value);
+  }
 
-      async del(key) {
-        this.client.del(key);
-      }
+  async del(key) {
+    this.client.del(key);
+  }
 }
 
 const redisClient = new RedisClient();
