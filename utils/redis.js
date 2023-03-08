60a30f893ebcceaa13dbb4d4dd8ebafb868b472e
Menor25 <ajirimenor@gmail.com>
2023-03-08 18:57:23 +0100

    Inside the folder utils, create a file redis.js that contains the class RedisClient

---
+++ utils/redis.js
@@ -0,0 +1,33 @@
+import createClient from 'redis';
+
+
+class RedisClient {
+    constructor() {
+        const host = process.env.RD_HOST || 'localhost';
+        const port = process.env.RD_PORT || '6379';
+        this.client = createClient({ host, port });
+        this.get = promisify(this.client.get).bind(this.client);
+        this.client.on('error', (err) => console.log(err.message));
+      }
+
+    
+      isAlive() {
+        return this.client.connected;
+      }
+
+      async get(key) {
+        const value = await this.get(key);
+        return value;
+      }
+
+      async set(key, value, duration) {
+        await this.client.setex(key, duration, value);
+      }
+
+      async del(key) {
+        this.client.del(key);
+      }
+}
+
+const redisClient = new RedisClient();
+module.exports = redisClient;
